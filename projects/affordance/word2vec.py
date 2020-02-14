28b79f126c88012df3c8858a5444cf212b6cc3e2
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-02-14 11:56:22 -0800

    remove unnecessary import

--- projects/affordance/word2vec.py
+++ projects/affordance/word2vec.py
@@ -13,8 +13,8 @@ from nltk.corpus import wordnet as wn, words
 ROOT_DIRECTORY = dirname(dirname(dirname(realpath(__file__))))
 sys.path.insert(0, ROOT_DIRECTORY)
 
-from projects.affordance.utils import *
-from research.knowledge_base import KnowledgeFile, Value
+from utils import *
+from research.knowledge_base import KnowledgeFile
 
 
 UMBEL_KB_PATH = join_path(ROOT_DIRECTORY, 'data/kbs/umbel-concepts-typology.rdfsqlite')
